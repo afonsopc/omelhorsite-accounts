f50e7324b2cb32dfcd596ae5afbf527aa866b0b3
Afonso Coutinho <afonsopcastro@gmail.com>
2023-10-12 23:49:55 +0100

    adicioandas lignausges

--- website-main/src/src/translations.tsx
+++ website-main/src/src/translations.tsx
@@ -131,6 +131,272 @@ const portugueseDictionary: Dictionary = {
     automatic: "Automático",
     changeTheme: "Alterar o Tema"
 }
+const frenchDictionary: Dictionary = {
+    unknownError: "Erreur inconnue",
+    email: "E-mail",
+    enterEmail: "Entrez votre e-mail",
+    enterNewEmail: "Entrez votre nouveau e-mail",
+    password: "Mot de passe",
+    enterPassword: "Entrez votre mot de passe",
+    dont_have_account_yet: "Vous n'avez pas encore de compte ?",
+    already_have_account: "Vous avez déjà un compte ?",
+    signIn: "Se connecter",
+    signUp: "S'inscrire",
+    close: "Fermer",
+    copyright: "Afonso Coutinho, 2023",
+    logotype: "Logotype",
+    home: "Accueil",
+    about: "À propos",
+    pricing: "Tarification",
+    account: "Compte",
+    myDrive: "Mon Drive",
+    settings: "Paramètres",
+    logout: "Se déconnecter",
+    changeEmail: "Changer l'e-mail",
+    change: "Changer",
+    newEmail: "Nouvel e-mail",
+    changePassword: "Changer le mot de passe",
+    newPassword: "Nouveau mot de passe",
+    enterNewPassword: "Entrez votre nouveau mot de passe",
+    show: "Afficher",
+    hide: "Masquer",
+    changeLanguage: "Changer de langue",
+    language: "Langue",
+    deleteAccount: "Supprimer le compte",
+    deleteAccountConfirmation: "Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible.",
+    homeDescription: "Nous offrons jusqu'à 50 Go d'espace de stockage pour chaque utilisateur. Stockez et accédez à vos fichiers en toute facilité, sécurité et commodité. Accédez à vos fichiers sur tous vos appareils, partagez des documents importants et évitez la perte d'informations !",
+    homeTitle: "Votre Cloud Personnel",
+    free: "Gratuit",
+    premium: "Premium",
+    monthsShort: "mois",
+    freePlan1: "20 Go de stockage",
+    premiumPlan1: "50 Go de stockage",
+    unavailable: "Indisponible",
+    freePlanPrice: "0",
+    premiumPlanPrice: "5",
+    websiteName: "Le meilleur site web",
+    enterUsername: "Entrez votre nom d'utilisateur",
+    username: "Nom d'utilisateur",
+    invalidConfirmationCode: "Code de confirmation invalide",
+    confirmationCodeModalTitle: "Code de Confirmation",
+    toConfirmInsertConfirmationCodeBellow: "Pour confirmer, saisissez le code de confirmation ci-dessous",
+    confirmationEmailSent: "Un code de confirmation a été envoyé à l'e-mail précédemment fourni",
+    cancel: "Annuler",
+    confirm: "Confirmer",
+    cancelPendingConfirmations: "Annuler les confirmations en attente",
+    pendingConfirmationsCanceledSuccessfully: "Confirmations en attente annulées avec succès",
+    enterNewUsername: "Entrez le nouveau nom d'utilisateur",
+    newUsername: "Nouveau nom d'utilisateur",
+    changeUsername: "Changer le nom d'utilisateur",
+    currentUsername: "Nom d'utilisateur actuel",
+    currentEmail: "E-mail actuel",
+    dark: "Sombre",
+    light: "Clair",
+    automatic: "Automatique",
+    changeTheme: "Changer de thème",
+    confirmationEmailSentToOriginal: "Un code de confirmation a été envoyé à l'e-mail d'origine.",
+    confirmationEmailSentToNew: "Un code de confirmation a été envoyé au nouvel e-mail."
+}
+
+const spanishDictionary: Dictionary = {
+    unknownError: "Error desconocido",
+    email: "Correo electrónico",
+    enterEmail: "Introduce tu correo electrónico",
+    enterNewEmail: "Introduce tu nuevo correo electrónico",
+    password: "Contraseña",
+    enterPassword: "Introduce tu contraseña",
+    dont_have_account_yet: "¿Todavía no tienes una cuenta?",
+    already_have_account: "¿Ya tienes una cuenta?",
+    signIn: "Iniciar sesión",
+    signUp: "Registrarse",
+    close: "Cerrar",
+    copyright: "Afonso Coutinho, 2023",
+    logotype: "Logotipo",
+    home: "Inicio",
+    about: "Acerca de",
+    pricing: "Precios",
+    account: "Cuenta",
+    myDrive: "Mi Drive",
+    settings: "Configuración",
+    logout: "Cerrar sesión",
+    changeEmail: "Cambiar correo electrónico",
+    change: "Cambiar",
+    newEmail: "Nuevo correo electrónico",
+    changePassword: "Cambiar contraseña",
+    newPassword: "Nueva contraseña",
+    enterNewPassword: "Introduce tu nueva contraseña",
+    show: "Mostrar",
+    hide: "Ocultar",
+    changeLanguage: "Cambiar idioma",
+    language: "Idioma",
+    deleteAccount: "Borrar cuenta",
+    deleteAccountConfirmation: "¿Estás seguro de que deseas borrar tu cuenta? Esta acción es irreversible.",
+    homeDescription: "Ofrecemos hasta 50 GB de espacio de almacenamiento para cada usuario. Almacena y accede a tus archivos con facilidad, seguridad y comodidad. Accede a tus archivos en todos tus dispositivos, comparte documentos importantes y evita la pérdida de información.",
+    homeTitle: "Tu Nube Personal",
+    free: "Gratis",
+    premium: "Premium",
+    monthsShort: "mes",
+    freePlan1: "20 GB de almacenamiento",
+    premiumPlan1: "50 GB de almacenamiento",
+    unavailable: "No disponible",
+    freePlanPrice: "0",
+    premiumPlanPrice: "5",
+    websiteName: "El Mejor Sitio Web",
+    enterUsername: "Introduce tu nombre de usuario",
+    username: "Nombre de usuario",
+    invalidConfirmationCode: "Código de confirmación inválido",
+    confirmationCodeModalTitle: "Código de Confirmación",
+    toConfirmInsertConfirmationCodeBellow: "Para confirmar, introduce el código de confirmación en el campo de abajo",
+    confirmationEmailSent: "Se ha enviado un código de confirmación al correo electrónico proporcionado previamente",
+    cancel: "Cancelar",
+    confirm: "Confirmar",
+    cancelPendingConfirmations: "Cancelar confirmaciones pendientes",
+    pendingConfirmationsCanceledSuccessfully: "Confirmaciones pendientes canceladas con éxito",
+    enterNewUsername: "Introduce el nuevo nombre de usuario",
+    newUsername: "Nuevo nombre de usuario",
+    changeUsername: "Cambiar nombre de usuario",
+    currentUsername: "Nombre de usuario actual",
+    currentEmail: "Correo electrónico actual",
+    dark: "Modo Oscuro",
+    light: "Modo Claro",
+    automatic: "Modo Automático",
+    changeTheme: "Cambiar Tema",
+    confirmationEmailSentToOriginal: "Se ha enviado un código de confirmación al correo electrónico original.",
+    confirmationEmailSentToNew: "Se ha enviado un código de confirmación al nuevo correo electrónico."
+}
+
+const germanDictionary: Dictionary = {
+    unknownError: "Unbekannter Fehler",
+    email: "E-Mail",
+    enterEmail: "Gib deine E-Mail-Adresse ein",
+    enterNewEmail: "Gib deine neue E-Mail-Adresse ein",
+    enterNewPassword: "Gib dein neues Passwort ein",
+    show: "Anzeigen",
+    hide: "Verbergen",
+    changeEmail: "E-Mail ändern",
+    change: "Ändern",
+    changePassword: "Passwort ändern",
+    newEmail: "Neue E-Mail",
+    password: "Passwort",
+    enterPassword: "Gib dein Passwort ein",
+    newPassword: "Neues Passwort",
+    dont_have_account_yet: "Noch kein Konto?",
+    already_have_account: "Bereits ein Konto?",
+    signIn: "Anmelden",
+    signUp: "Registrieren",
+    close: "Schließen",
+    copyright: "Afonso Coutinho, 2023",
+    logotype: "Logo",
+    home: "Startseite",
+    about: "Über uns",
+    pricing: "Preise",
+    account: "Konto",
+    myDrive: "Mein Laufwerk",
+    settings: "Einstellungen",
+    logout: "Abmelden",
+    changeLanguage: "Sprache ändern",
+    language: "Sprache",
+    deleteAccount: "Konto löschen",
+    deleteAccountConfirmation: "Sind Sie sicher, dass Sie Ihr Konto löschen möchten? Dieser Vorgang kann nicht rückgängig gemacht werden.",
+    homeDescription: "Wir bieten jedem Benutzer bis zu 50 GB Speicherplatz an. Speichern und greifen Sie einfach, sicher und bequem auf Ihre Dateien zu. Greifen Sie auf Ihre Dateien auf all Ihren Geräten zu, teilen Sie wichtige Dokumente und vermeiden Sie Datenverlust!",
+    homeTitle: "Ihre persönliche Cloud",
+    free: "Kostenlos",
+    premium: "Premium",
+    monthsShort: "Monat",
+    freePlan1: "20 GB Speicherplatz",
+    premiumPlan1: "50 GB Speicherplatz",
+    unavailable: "Nicht verfügbar",
+    freePlanPrice: "0",
+    premiumPlanPrice: "5",
+    websiteName: "Die beste Website",
+    enterUsername: "Gib deinen Benutzernamen ein",
+    username: "Benutzername",
+    invalidConfirmationCode: "Ungültiger Bestätigungscode",
+    confirmationCodeModalTitle: "Bestätigungscode",
+    toConfirmInsertConfirmationCodeBellow: "Geben Sie den Bestätigungscode in das unten stehende Textfeld ein, um zu bestätigen.",
+    confirmationEmailSent: "Ein Bestätigungscode wurde an die zuvor angegebene E-Mail-Adresse gesendet.",
+    cancel: "Abbrechen",
+    cancelPendingConfirmations: "Ausstehende Bestätigungen abbrechen",
+    pendingConfirmationsCanceledSuccessfully: "Ausstehende Bestätigungen erfolgreich abgebrochen!",
+    confirm: "Bestätigen",
+    enterNewUsername: "Gib deinen neuen Benutzernamen ein",
+    newUsername: "Neuer Benutzername",
+    changeUsername: "Benutzernamen ändern",
+    currentUsername: "Aktueller Benutzername",
+    currentEmail: "Aktuelle E-Mail",
+    dark: "Dunkel",
+    light: "Hell",
+    automatic: "Automatisch",
+    changeTheme: "Design ändern",
+    confirmationEmailSentToOriginal: "Bestätigungs-E-Mail wurde an die ursprüngliche E-Mail gesendet.",
+    confirmationEmailSentToNew: "Bestätigungs-E-Mail wurde an die neue E-Mail gesendet."
+}
+const englishDictionary: Dictionary = {
+    unknownError: "Unknown error",
+    email: "Email",
+    enterEmail: "Enter your email",
+    enterNewEmail: "Enter your new email",
+    password: "Password",
+    enterPassword: "Enter your password",
+    dont_have_account_yet: "Don't have an account yet?",
+    already_have_account: "Already have an account?",
+    signIn: "Sign In",
+    signUp: "Sign Up",
+    close: "Close",
+    copyright: "Afonso Coutinho, 2023",
+    logotype: "Logotype",
+    home: "Home",
+    about: "About",
+    pricing: "Pricing",
+    account: "Account",
+    myDrive: "My Drive",
+    settings: "Settings",
+    logout: "Logout",
+    changeEmail: "Change Email",
+    change: "Change",
+    newEmail: "New Email",
+    changePassword: "Change Password",
+    newPassword: "New Password",
+    enterNewPassword: "Enter your new password",
+    show: "Show",
+    hide: "Hide",
+    changeLanguage: "Change Language",
+    language: "Language",
+    deleteAccount: "Delete Account",
+    deleteAccountConfirmation: "Are you sure you want to delete your account? This action is irreversible.",
+    homeDescription: "We offer up to 50GB of storage space for each user. Store and access your files with ease, security, and convenience. Access your files on all your devices, share important documents, and prevent data loss!",
+    homeTitle: "Your Personal Cloud",
+    free: "Free",
+    premium: "Premium",
+    monthsShort: "mo",
+    freePlan1: "20 GB of storage",
+    premiumPlan1: "50 GB of storage",
+    unavailable: "Unavailable",
+    freePlanPrice: "0",
+    premiumPlanPrice: "5",
+    websiteName: "The Best Website",
+    enterUsername: "Enter your username",
+    username: "Username",
+    invalidConfirmationCode: "Invalid confirmation code",
+    confirmationCodeModalTitle: "Confirmation Code",
+    toConfirmInsertConfirmationCodeBellow: "To confirm, enter the confirmation code below",
+    confirmationEmailSent: "A confirmation code has been sent to the email provided previously",
+    cancel: "Cancel",
+    confirm: "Confirm",
+    cancelPendingConfirmations: "Cancel pending confirmations",
+    pendingConfirmationsCanceledSuccessfully: "Pending confirmations canceled successfully",
+    enterNewUsername: "Enter your new username",
+    newUsername: "New username",
+    changeUsername: "Change username",
+    currentUsername: "Current username",
+    currentEmail: "Current email",
+    dark: "Dark",
+    light: "Light",
+    automatic: "Automatic",
+    changeTheme: "Change Theme",
+    confirmationEmailSentToOriginal: "A confirmation code has been sent to the original email.",
+    confirmationEmailSentToNew: "A confirmation code has been sent to the new email."
+};
 
 export interface Language {
     code: string;
@@ -146,24 +412,58 @@ const portuguese: Language = {
     dictionary: portugueseDictionary
 }
 
+const spanish: Language = {
+    code: 'spanish',
+    name: 'Español',
+    flagPath: '/es.svg',
+    dictionary: spanishDictionary
+}
+
+const french: Language = {
+    code: 'french',
+    name: 'Français',
+    flagPath: '/fr.svg',
+    dictionary: frenchDictionary
+}
+
+const german: Language = {
+    code: 'german',
+    name: 'Deutsch',
+    flagPath: '/de.svg',
+    dictionary: germanDictionary
+}
+
+const english: Language = {
+    code: 'english',
+    name: 'English',
+    flagPath: '/en.svg',
+    dictionary: englishDictionary
+}
+
 export interface Languages {
     portuguese: Language;
+    spanish: Language;
+    french: Language;
+    german: Language;
+    english: Language;
 }
 
 export const languages: Languages = {
     portuguese,
+    spanish,
+    french,
+    german,
+    english,
 }
 
 export function getLanguage() {
     const language = localStorage.getItem("language") ? localStorage.getItem("language") : detectAndSetLanguage();
 
     if (language === "portuguese") { return languages.portuguese }
-    // if (language === "spanish") { return languages.spanish }
-    // if (language === "french") { return languages.french }
-    // if (language === "german") { return languages.german }
-    // else { return languages.english };
-    return languages.portuguese
-
+    if (language === "spanish") { return languages.spanish }
+    if (language === "french") { return languages.french }
+    if (language === "german") { return languages.german }
+    else { return languages.english };
 }
 
 export function detectAndSetLanguage(): string {
